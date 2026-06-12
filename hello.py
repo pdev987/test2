ed0e17a30d43d3f97246380d1777a51e32700e76
pdev <pdev@email.com>
2026-06-12 19:10:22 +0530

    build: added some code

--- hello.py
+++ hello.py
@@ -1,3 +1,22 @@
-print("Hello world")
-print("Hello world")
+def check_is_integer(x):
+    """
+    check x is integer or not
+    """
+    return isinstance(x, int)
+def a_greater_than_b(a,b):
+    """
+    this is temporary function that is comparing
+    two variables a and b if they are numbers
+    compare them are return the 
+    a is greater than b or not
+    """
+    if check_is_integer(a) and check_is_integer(b):
+            return a > b
+    else:
+        # if they are not number 
+        # return None
+        return None
 
+if __name__ == "__main__":
+    print(a_greater_than_b(10, 1))
+        
